d80e5200fa843731f6018b3195aeb1fa71a74dba
Eduardo Soares <eduschadesoares@hotmail.com>
2019-06-20 15:05:10 -0300

    Oficina de Jogos Digitais

---
+++ Fat Bird 2.0/Assets/Scripts/GameControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl Instance;
+
+    public float scrollSpeed = -1.5f;
+    public bool isGameOver = false;
+    private int score = 0;
+
+    public Text scoreText;
+    public GameObject gameOverText;
+
+	// Use this for initialization
+	void Awake () {
+		if(Instance == null)
+        {
+            Instance = this;
+        } else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isGameOver && Input.GetButtonDown("Jump"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+    public void Score()
+    {
+        if (isGameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score;
+        //scrollSpeed -= (float)0.3;
+    }
+
+    public void Die()
+    {
+        isGameOver = true;
+        gameOverText.SetActive(true);
+    }
+}
